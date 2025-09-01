0cde1bb8d66974f2c1e5745c81b2959cd58fa9d3
Abderahmane Bouziane <bobmatt911@gmail.com>
2025-09-01 14:25:05 -0400

    Delete examples directory

--- examples/next/lib/tanstack.ts
+++
@@ -1,7 +0,0 @@
-import { QueryKey } from "@tanstack/react-query";
-
-export function getEntriesQueryKey<T extends string | null>(
-  sessionId: T
-): QueryKey {
-  return ["entries", sessionId] as const;
-}
